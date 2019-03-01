e64c1c1ee89170138f8791ed51f7ede77790a8cc
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-01 14:26:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock){
-    const range = Math.abs(startBlock.slice(0,2) - endBlock.slice(0,2))
-
-
-
-    const diffRange = Math.abs(blockRange - range)
-    // let distance = Math.abs(end-start);
-    // let range = blockRange - distance;
+    let start = parseInt(startBlock);
+    let end = parseInt(endBlock);
+      let range = Math.abs(startBlock.slice(0,2) - endBlock.slice(0,2))
+      let distance = Math.abs(blockRange - range)
 
     if (range > 0){
       return `within range by ${range}`
