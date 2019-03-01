5207511b0d393aa7372706cc22512425f6968f1d
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-01 14:24:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock){
-    // let start = parseInt(startBlock);
-    // let end = parseInt(endBlock);
+    let start = parseInt(startBlock);
+    let end = parseInt(endBlock);
     let distance = Math.abs(end-start);
     let range = blockRange - distance;
 
