babbc805096657117e4166f2584c054c8f0ccfca
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-01 14:17:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,117 @@
-function produceDrivingRange(blockRange){
+function produceDrivingRange(blockRange) {
 
-  return function(startingBlock, endingBlock){
-    let start = parseInt(startingBlock);
-    let end = parseInt(endingBlock);
-    let distance = Math.abs(end - start);
-    let difference = blockRange - distance;
 
-    if (difference > 0) {
-      return `within range by ${difference}`
+
+  return function(startBlock, endBlock){
+
+
+
+    let start = parseInt(startBlock);
+
+
+
+    let end = parseInt(endBlock);
+
+
+
+    let distance = Math.abs(end-start);
+
+
+
+    let range = blockRange - distance;
+
+
+
+
+
+
+
+    if (range > 0){
+
+
+
+      return `within range by ${range}`
+
+
+
+    }else{
+
+
+
+      return `${Math.abs(range)} blocks out of range`
+
+
+
     }
 
-    else {
-      return `${Math.abs(difference)} blocks out of range`
-    }
+
+
   }
+
+
+
 }
 
-function produceTipCalculator(percentage){
-  return function(totalbill){
-    return percentage * totalbill;
-  }
+
+
+
+
+
+
+function produceTipCalculator(tip) {
+
+
+
+  return function(tripCost) {
+
+
+
+    return (tripCost * tip);
+
+
+
+  };
+
+
+
 }
+
+
+
+
+
+
 
 function createDriver(){
-  let driverId = 0;
-  return class Driver {
+
+
+
+  let driverId = 0
+
+
+
+  return class {
+
+
+
     constructor(name){
-      this.name = name;
-      this.id = ++driverId;
-    }
+
+
+
+    this.name = name;
+
+
+
+    this.id = driverId++;
+
+
+
   }
+
+
+
+  }
+
+
+
 }
