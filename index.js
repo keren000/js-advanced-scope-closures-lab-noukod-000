9526465648f5fb8bcca94ec212877adad9f5062c
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-01 14:36:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ function produceTipCalculator(calcul) {
   };
 }
 
-// function createDriver(){
-//   let driverId = 0
-//
-//   return class {
-//     constructor(name){
-//     this.name = name;
-//     this.id = driverId++;
-//   }
-//   }
+function createDriver(){
+  let driverId = 0
+
+  return class {
+    constructor(name){
+    this.name = name;
+    this.id = driverId++;
+  }
+  }
 }
