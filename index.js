bab1e06f17f19137b98325eda308734c56d3752d
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-01 14:15:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,121 +1,33 @@
 function produceDrivingRange(blockRange){
 
-
-
   return function(startingBlock, endingBlock){
-
-
-
-
-
-
-
     let start = parseInt(startingBlock);
-
-
-
     let end = parseInt(endingBlock);
-
-
-
     let distance = Math.abs(end - start);
-
-
-
     let difference = blockRange - distance;
-
-
-
-
-
-
-
+    
     if (difference > 0) {
-
-
-
       return `within range by ${difference}`
-
-
-
-    } else {
-
-
-
+    } 
+    
+    else {  
       return `${Math.abs(difference)} blocks out of range`
-
-
-
     }
-
-
-
   }
-
-
-
 }
-
-
-
-
-
-
 
 function produceTipCalculator(percentage){
-
-
-
   return function(totalbill){
-
-
-
     return percentage * totalbill;
-
-
-
   }
-
-
-
 }
 
-
-
-
-
-
-
 function createDriver(){
-
-
-
   let driverId = 0;
-
-
-
   return class Driver {
-
-
-
     constructor(name){
-
-
-
       this.name = name;
-
-
-
       this.id = ++driverId;
-
-
-
     }
-
-
-
   }
-
-
-
 }
